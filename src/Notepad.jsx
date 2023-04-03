c791915b7310a73bf18aa3bd92982fa5a620788c
colemilne54 <colemilne54@gmail.com>
2023-04-03 10:50:58 -0600

    Base functionality working

--- src/Notepad.jsx
+++ src/Notepad.jsx
@@ -1,24 +1,36 @@
 import React, { useState } from 'react';
+import randomWords from 'random-words';
+
+function GeneratePrompt() {
+  const min = 3;
+  const max = 10;
+  return randomWords(Math.floor(Math.random() * (max - min + 1) + min)).join(' ');
+}
+
+const prompt = GeneratePrompt();
 
 export default function Notepad() {
-  const [postContent, setPostContent] = useState(''); // Declare a state variable...
+  const [postContent, setPostContent] = useState('');
 
   function handleChange(input) {
-    // console.log(input);
     var randChar = Math.random().toString(36).slice(2, 3);
     
     if (Math.random() < .2) {
-      setPostContent(input + randChar);
+      setPostContent(postContent + randChar);
     } else {
       setPostContent(input);
     }
-    
   }
 
   return (
+    <>
+    <p>type the following:</p>
+    <p>{`${prompt}`}</p>
     <textarea
       value={postContent}
       onChange={e => handleChange(e.target.value)}
     />
+    <p>{prompt == postContent && 'nice work'}</p>
+    </>
   );
 }
